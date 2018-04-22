d719d2eaa2552e2a8608c321099d8d4a387f2b9c
tatsuokun <j20.at09303@gmail.com>
2018-04-22 16:13:46 +0900

    remove dupulication

--- run.sh
+++
@@ -1,3 +0,0 @@
-python -m DeNSe
-perl DeNSe/eval08.pl -g DeNSe/data/dev_gold -s DeNSe/data/dev_pred > result_dev.txt
-perl DeNSe/eval08.pl -g DeNSe/data/test_gold -s DeNSe/data/test_pred > result_test.txt
